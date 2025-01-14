e8fa924c679caa07787dda1190254ced40b9c7c6
Krampus B. <tiobarryx@gmail.com>
2025-01-14 03:57:58 +0100

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const videoURL = "https://youtu.be/4X4uckVyk9o?feature=shared";
 const outputPath = path.join(__dirname, "public", "video_prueba.mp4");
 
 // Comando de yt-dlp para descargar el video
-const command = `yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 -o "${outputPath}" ${videoURL}`;
+const command = `yt-dlp -o "${outputPath}" ${videoURL}`;
 
 // Ruta principal con el menú
 app.get("/", (req, res) => {
+  const downloadLink = fs.existsSync(outputPath) ? `<a href="/public/video_prueba.mp4" style="font-size:20px;">Descargar Video</a>` : '';
   res.send(`
     <html>
       <head>
@@ -24,7 +25,8 @@ app.get("/", (req, res) => {
       <body>
         <h1>Bienvenido a la API Operacion Marshall</h1>
         <p>Seleccione una opción:</p>
-        <a href="/public-files" style="font-size:20px;">Ver archivos en 'public'</a>
+        <a href="/public-files" style="font-size:20px;">Ver archivos en 'public'</a><br/><br/>
+        ${downloadLink}
       </body>
     </html>
   `);
@@ -58,35 +60,16 @@ app.get("/public-files", (req, res) => {
           <ul>${fileList}</ul>
           <br />
           <a href="/" style="font-size:20px;">Volver al menú principal</a>
-          <br />
-          <a href="/download" style="font-size:20px;">Descargar video nuevamente</a>
         </body>
       </html>
     `);
   });
 });
 
-// Ruta para descargar el video directamente (enlace directo)
-app.get("/download", (req, res) => {
-  // Intentar descargar el video cada vez que se accede a esta ruta
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      logger.error(`Error al descargar el video: ${error.message}`);
-      return res.status(500).send("Hubo un error al descargar el video.");
-    }
-    if (stderr) {
-      logger.error(`stderr: ${stderr}`);
-      return res.status(500).send("Hubo un error al descargar el video.");
-    }
-    logger.info(`Video descargado exitosamente: ${stdout}`);
-    res.redirect("/public-files"); // Redirigir a la lista de archivos
-  });
-});
-
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, "public")));
 
-// Ejecutar la descarga del video cuando se encienda la API
+// Ejecutar la descarga del video siempre cuando se inicie la API
 exec(command, (error, stdout, stderr) => {
   if (error) {
     logger.error(`Error al descargar el video: ${error.message}`);
@@ -99,7 +82,6 @@ exec(command, (error, stdout, stderr) => {
   logger.info(`Video descargado exitosamente: ${stdout}`);
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   logger.info(`Servidor corriendo en http://localhost:${PORT}`);
 });
